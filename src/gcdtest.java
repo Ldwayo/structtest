4bc86746b5c542962751d0dbdb7d78eced3454b5
Ldwayo <lindongwangyu@qq.com>
2018-03-10 20:45:36 +0800

    辗转相除法

--- src/gcdtest.java
+++ src/gcdtest.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class gcdtest {
     public int gcd(int a,int b){
         if (b == 0) return a;
         return gcd(b,a%b);
     }
 
-    public static void main(String[] args) {
-        int ans = new gcdtest().gcd(12,30);
+    public static void main(String[] args) {    //最大公约数
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a:");
+        int a = scanner.nextInt();
+        System.out.print("b:");
+        int b = scanner.nextInt();
+        int ans = new gcdtest().gcd(a,b);
         System.out.println(ans);
     }
 }
